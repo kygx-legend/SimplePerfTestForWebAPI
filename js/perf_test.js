daf684e821e3a7515047ce72b2c3df6737d2cc4d
legendlee1314 <legendlee1314@gmail.com>
2013-07-20 01:26:47 +0800

    separate visualization and test

--- js/perf_test.js
+++ js/perf_test.js
@@ -4,15 +4,19 @@
     Last Modified Date: 2013-07-19
     Module: Perf_Test
     Description:
-        XWalk API Performance Test
+        API Performance Test
         1. To generate test strings arrays and ints arrays with linear length.
-        2. To test one function of API by loading generated parameters. There are two subtasks:
+        2. To test one function of API by loading generated parameters.
+            There are two subtasks:
             a) Get the time of writing and reading to show the speed performance.
             b) Test and verify the integrity and accuracy.
             c) * More in depth, make the above visualization.
+        3. Two main test tasks:
+            one time for large data; more times for small data;
+        4. Test object are `set_property` and `get_property`. \
+            by one large string or one string array with more small strings.
    */
 
-// alert('ttt');
 // goal test function
 function goal_test_function(data){
     this.data = data;
@@ -42,25 +46,50 @@ function perf_test(api_function, data){
     time.midtime = new Date().getTime();
     var newdata = api_function.data;
     time.endtime = new Date().getTime();
+    //@todo
     //validate(data, newdata);
     return time
+}
+
+// visualization
+function visualize(result){
+    if(result != null){
+        var canvas = document.getElementById("myCanvas");
+        var context = canvas.getContext("2d");
+        var k = 20;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.lineWidth = 4;
+        context.beginPath();
+        context.moveTo(0, 0);
+        for(x in result){
+            context.lineTo(k, result[x].midtime - result[x].starttime);
+            k += 10;
+        }
+        context.strokeStyle = '#ff0000';
+        context.stroke();
+        context.closePath();
+        context.beginPath();
+        var k = 20;
+        context.moveTo(0, 0);
+        for(x in result){
+            context.lineTo(k, result[x].endtime - result[x].midtime);
+            k += 10;
+        }
+        context.strokeStyle = '#0000ff';
+        context.stroke();
+        context.closePath();
+    }
 }
 
 // main loop of `perf_test`
 function perf_test_main(){
     var data = data_generate();
-    var canvas = document.getElementById("myCanvas");
-    var context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-    context.moveTo(0, 0);
-    var k = 10;
+    var result = new Array();
     for(d in data){
         time = perf_test(goal_test_function, d);
-        context.lineTo(time.endtime - time.starttime, k);
-        k += 10;
+        result.push(time);
     }
-    context.stroke();
+    visualize(result);
 }
 
 // call main function
