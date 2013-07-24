007e28b2bbf85f6bd2a60d0955912050df20e74f
legendlee1314 <legendlee1314@gmail.com>
2013-07-24 16:54:07 +0800

    fixed

--- js/perf_test.js
+++ js/perf_test.js
@@ -1,21 +1,21 @@
 /*
-Author: LegendLee(legendlee1314@gmail.com)
-Date: 2013-07-19
-Last Modified Date: 2013-07-24
-Module: Perf_Test
-Description:
-    API Object Performance Test
-    1. To generate test strings arrays and ints arrays with linear length.
-    2. To test one function of API by loading generated parameters.
-        There are two subtasks:
-        a) Get the time of writing and reading to show the speed performance.
-        b) Test and verify the integrity and accuracy.
-        c) * More in depth, make the above visualization.
-    3. Two main test tasks:
-        one time for large data; more times for small data;
-    4. Test object are `setProperty` and `getProperty`. \
-        by one large string or one string array with more small strings.
-*/
+ *Author: LegendLee(legendlee1314@gmail.com)
+ *Date: 2013-07-19
+ *Last Modified Date: 2013-07-24
+ *Module: Perf_Test
+ *Description:
+ *    API Object Performance Test
+ *    1. To generate test strings arrays and ints arrays with linear length.
+ *    2. To test one function of API by loading generated parameters.
+ *        There are two subtasks:
+ *        a) Get the time of writing and reading to show the speed performance.
+ *        b) Test and verify the integrity and accuracy.
+ *        c) * More in depth, make the above visualization.
+ *    3. Two main test tasks:
+ *        one time for large data; more times for small data;
+ *    4. Test object are `setProperty` and `getProperty`. \
+ *        by one large string or one string array with more small strings.
+ */
 
 
 // xwalk test goal example
